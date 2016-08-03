e8479f119ef337d8d5d64f0028e333597d7132d7
Dimeji A. Ajasin <dimejiajasin@gmail.com>
2016-08-02 19:17:16 -0700

    Update the other html link

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ var express = require('express');
 var app = express();
 var port = 5000;
 
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+
 app.get('/', function (req, res) {
-    res.send('Hello there Dimeji');
+    res.render('index');
 });
 
 
